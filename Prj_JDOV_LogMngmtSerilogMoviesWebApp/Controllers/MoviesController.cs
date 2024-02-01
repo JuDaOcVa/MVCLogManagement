b472763fb6f93407c17cfbc5c71fa874b5879299
JuDaOcVa <juandavidocampo80@gmail.com>
2024-02-01 09:06:34 -0500

    Init commit repository with code

---
+++ Prj_JDOV_LogMngmtSerilogMoviesWebApp/Controllers/MoviesController.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Prj_JDOV_LogMngmtSerilogMoviesWebApp.Models;
+
+namespace Prj_JDOV_LogMngmtSerilogMoviesWebApp.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly BdJdovLaboratorio4Context _context;
+        private readonly ILogger<MoviesController> _logger;
+
+        public MoviesController(BdJdovLaboratorio4Context context, ILogger<MoviesController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        // GET: Movies
+        public async Task<IActionResult> Index()
+        {
+            _logger.LogInformation("[Movies-Index]:{0} : Listado de ítems de Movies.", DateTime.Now);
+
+            if (_context.Movies != null)
+            {
+                return View(await _context.Movies.ToListAsync());
+            }
+            else
+            {
+                return Problem("Entity set 'BdJdovLaboratorio4Context.Movies' is null.");
+            }
+        }
+
+        // GET: Movies/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Movies == null)
+            {
+                return NotFound();
+            }
+
+            var movie = await _context.Movies
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            return View(movie);
+        }
+
+        // GET: Movies/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Movies/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,MovName,MovDirector,MovType,MovYear,MovWorldWideGross,MovDateCreated")] Movie movie)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(movie);
+                await _context.SaveChangesAsync();
+                _logger.LogInformation("[Movies-Create]:{0} : Item {1} ha sido creado en la tabla Movies.", DateTime.Now, movie.MovName);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(movie);
+        }
+
+        // GET: Movies/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Movies == null)
+            {
+                _logger.LogError("[Movies-Update]:{0} : Item {1} no esta disponible para realizar una operación de actualización de Movies.", DateTime.Now, id);
+                return NotFound();
+            }
+
+            var movie = await _context.Movies.FindAsync(id);
+            if (movie == null)
+            {
+                _logger.LogWarning("[Movies-Update]:{0} : Item no puede ser vacío para la operación de Actualización de Movie.", DateTime.Now);
+                return NotFound();
+            }
+            return View(movie);
+        }
+
+        // POST: Movies/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,MovName,MovDirector,MovType,MovYear,MovWorldWideGross,MovDateCreated")] Movie movie)
+        {
+            if (id != movie.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(movie);
+                    await _context.SaveChangesAsync();
+                    _logger.LogInformation("[Movies-Update]:{0} : Item {1} ha sido Actualizado en la tabla Movies.", DateTime.Now, movie.MovName);
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MovieExists(movie.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(movie);
+        }
+
+        // GET: Movies/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Movies == null)
+            {
+                return NotFound();
+            }
+
+            var movie = await _context.Movies
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            return View(movie);
+        }
+
+        // POST: Movies/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Movies == null)
+            {
+                return Problem("Entity set 'BdJdovLaboratorio4Context.Movies'  is null.");
+            }
+            var movie = await _context.Movies.FindAsync(id);
+            if (movie != null)
+            {
+                _context.Movies.Remove(movie);
+                _logger.LogInformation("[Movies-Delete]:{0} : Item {1} ha sido Borrado de la tabla Movies.", DateTime.Now, id);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MovieExists(int id)
+        {
+          return (_context.Movies?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
